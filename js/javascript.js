77c0e55a6d5b85cc4177e860791b4634dcd88d8c
SuBorgesMoreira <suzanaborges070@gmail.com>
2018-09-13 22:18:36 -0300

    formulario de pessoa fisica

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,57 @@
-function.validacao(){
-	if(document.form.nome.value==""){
-	  alert("Por favor, preencha o campo nome.");
-	  document.form.nome.focus();
-	  return false;
+function VerificaCPF () {
+if (vercpf(document.frmcpf.cpf.value)) 
+{document.frmcpf.submit();}else 
+{errors="1";if (errors) alert('CPF NÃO VÁLIDO');
+document.retorno = (errors == '');}}
+function vercpf (cpf) 
+{if (cpf.length != 11 || cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999")
+return false;
+add = 0;
+for (i=0; i < 9; i ++)
+add += parseInt(cpf.charAt(i)) * (10 - i);
+rev = 11 - (add % 11);
+if (rev == 10 || rev == 11)
+rev = 0;
+if (rev != parseInt(cpf.charAt(9)))
+return false;
+add = 0;
+for (i = 0; i < 10; i ++)
+add += parseInt(cpf.charAt(i)) * (11 - i);
+rev = 11 - (add % 11);
+if (rev == 10 || rev == 11)
+rev = 0;
+if (rev != parseInt(cpf.charAt(10)))
+return false;
+alert('O CPF INFORMADO É VÁLIDO.');return true;
 }
-	if(document.form.cpf.id==""){
-	   alert("Por favor, insira seu cpf");
-       document.form.cpf.focus();
-	   return false;
-}       
 
-	if(document.form.email.value==""|| document.form.email.value.indeOf('@')==-1|| document.form.email.value.indeOf('.')==-1{
-	  alert("Por favor, digite um endereço de email válido!");
-	  document.form.email.focus();
-	  return false;
+ function mascara(t, mask){
+ var i = t.value.length;
+ var saida = mask.substring(1,0);
+ var texto = mask.substring(i)
+ if (texto.substring(0,1) != saida){
+ t.value += texto.substring(0,1);
+ }
+ }
+ 
+ function mascara(t, mask){
+ var i = t.value.length;
+ var saida = mask.substring(1,0);
+ var texto = mask.substring(i);
+ if (texto.substring(0,1) != saida){
+ t.value += texto.substring(0,1);
+ }
+ }
+ 
+ function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
 }
+ 
+ 
