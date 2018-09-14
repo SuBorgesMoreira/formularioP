73c4b477e3e58a0887dde077b454842db6a440ee
SuBorgesMoreira <suzanaborges070@gmail.com>
2018-09-13 22:29:49 -0300

    formulario de pessoa fisica

--- js/javascript.js
+++ js/javascript.js
@@ -43,15 +43,4 @@ alert('O CPF INFORMADO É VÁLIDO.');return true;
  }
  }
  
- function formatar(mascara, documento){
-  var i = documento.value.length;
-  var saida = mascara.substring(0,1);
-  var texto = mascara.substring(i)
-  
-  if (texto.substring(0,1) != saida){
-            documento.value += texto.substring(0,1);
-  }
-  
-}
- 
  
